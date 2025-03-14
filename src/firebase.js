e3bf2c3283c7f5a8f8e64d68ea22c9e61645fdb5
Dhanushree Motanalira Somaiah <motanalirasomaiahd@cardiff.ac.uk>
2025-03-14 17:16:18 +0000

    app js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,15 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-};
-
+    apiKey: "AIzaSyD2Ye6SIAahxns2_rMjgCu_RGYrkTMOvdA",
+    authDomain: "finance-tracker-f8d4d.firebaseapp.com",
+    projectId: "finance-tracker-f8d4d",
+    storageBucket: "finance-tracker-f8d4d.firebasestorage.app",
+    messagingSenderId: "798336610717",
+    appId: "1:798336610717:web:d3e4c7aaeff164c1913f74",
+    measurementId: "G-BWQBBFK0S5"
+  };
+  
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
