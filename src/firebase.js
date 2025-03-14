b0cd6a6af7492d8bf51eafbf33f4dcbc6c5729a5
Dhanushree Somaiah <114977031+dhanu-shre3@users.noreply.github.com>
2025-03-14 17:46:07 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyD2Ye6SIAahxns2_rMjgCu_RGYrkTMOvdA",
-    authDomain: "finance-tracker-f8d4d.firebaseapp.com",
-    projectId: "finance-tracker-f8d4d",
-    storageBucket: "finance-tracker-f8d4d.firebasestorage.app",
-    messagingSenderId: "798336610717",
-    appId: "1:798336610717:web:d3e4c7aaeff164c1913f74",
-    measurementId: "G-BWQBBFK0S5"
-  };
   
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
